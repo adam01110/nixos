d0b2cce0d67c316f7d29624302ba71872463702d
adam01110 <alumail@protonmail.com>
2025-12-02 23:52:39 +0100

    hyprland update

--- home/desktop/hyprland/scripts/performant-mode.sh
+++ home/desktop/hyprland/scripts/performant-mode.sh
@@ -4,19 +4,21 @@
 HYPRPERFORMANTMODE=$(@hyprctl@ getoption animations:enabled | @gawk@ 'NR==1{print $2}')
 if [ "$HYPRPERFORMANTMODE" = 1 ]; then
   # turn off visual features and tighten layout.
-  @hyprctl@ --batch "\
-    keyword animations:enabled 0;\
-    keyword decoration:shadow:enabled 0;\
-    keyword decoration:blur:enabled 0;\
-    keyword general:gaps_in 0;\
-    keyword general:gaps_out 0;\
-    keyword general:border_size 1;\
-    keyword decoration:rounding 0;\
-    keyword decoration:active_opacity 1;\
-    keyword decoration:inactive_opacity 1"
+  SETTINGS=(
+    "animations:enabled 0"
+    "decoration:shadow:enabled 0"
+    "decoration:blur:enabled 0"
+    "general:gaps_in 0"
+    "general:gaps_out 0"
+    "general:border_size 1"
+    "decoration:rounding 0"
+    "decoration:active_opacity 1"
+    "decoration:inactive_opacity 1"
+    "windowrule opacity 1 override 1 override 1 override, ^(.*)$"
+  )
 
-  # make all windows fully opaque until reload.
-  @hyprctl@ keyword "windowrule opacity 1 override 1 override 1 override, ^(.*)$"
+  BATCH_COMMANDS=$(printf 'keyword %s;' "${SETTINGS[@]}")
+  @hyprctl@ --batch "$BATCH_COMMANDS"
 
   # notify the user about the active mode.
   @notifySend@ -u low -a "desktop" "Performant mode" "Enabled"
