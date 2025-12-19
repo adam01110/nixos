8b0caece2cc4bc2951a9c01b9e8af4897042b20a
adam01110 <alumail@protonmail.com>
2025-12-19 13:46:51 +0100

    add most printing drivers, hplip doesnt seem to want to work

--- system/services/printing.nix
+++ system/services/printing.nix
@@ -1,10 +1,31 @@
-{ ... }:
+{
+  pkgs,
+  ...
+}:
 
 # enable cups printing service.
+let
+  inherit (builtins) attrValues;
+in
 {
   services.printing = {
     enable = true;
     openFirewall = true;
+
+    # printer drivers packages.
+    drivers = attrValues {
+      inherit (pkgs)
+        foomatic-db-ppds
+        foomatic-db-ppds-withNonfreeDb
+        gutenprint
+        gutenprint-bin
+        splix
+        ;
+    };
+
+    # enable network printer discovery and share local queues by default.
+    browsing = true;
+    defaultShared = true;
   };
 
   # enable some fonts for ghostscript.
