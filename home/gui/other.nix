e67b695d5bb47df520dda0e044b7fd630d3db1a8
adam01110 <alumail@protonmail.com>
2026-01-29 23:20:35 +0100

    appimage, optional wooting, enable podman on laptop

--- home/gui/other.nix
+++ home/gui/other.nix
@@ -1,34 +1,41 @@
-{pkgs, ...}:
+{
+  osConfig,
+  lib,
+  pkgs,
+  ...
+}:
 # gui application and gui flatpak packages.
 let
   inherit (builtins) attrValues;
+  inherit (lib) optional;
 in {
   # install applications from nixpkgs and nur.
-  home.packages = attrValues {
-    inherit
-      (pkgs)
-      aseprite
-      bleachbit
-      bitwarden-desktop
-      decibels
-      gimp
-      gnome-calculator
-      helvum
-      loupe
-      obsidian
-      protonvpn-gui
-      showtime
-      upscayl
-      warehouse
-      wootility
-      heroic
-      devtoolbox
-      krita
-      ;
+  home.packages =
+    attrValues {
+      inherit
+        (pkgs)
+        aseprite
+        bleachbit
+        bitwarden-desktop
+        decibels
+        gimp
+        gnome-calculator
+        helvum
+        loupe
+        obsidian
+        protonvpn-gui
+        showtime
+        upscayl
+        warehouse
+        heroic
+        devtoolbox
+        krita
+        ;
 
-    inherit (pkgs.nur.repos.forkprince) helium-nightly;
-    inherit (pkgs.nur.repos.ymstnt) beeper;
-  };
+      inherit (pkgs.nur.repos.forkprince) helium-nightly;
+      inherit (pkgs.nur.repos.ymstnt) beeper;
+    }
+    ++ optional osConfig.hardware.wooting.enable pkgs.wootility;
 
   # install applications via flatpak.
   services.flatpak.packages = [
