f6a72b0c257e82637516c21af04d0688a0101ec6
adam01110 <alumail@protonmail.com>
2026-02-21 02:19:56 +0100

    fix neovim rpc

--- home/tui/neovim/presence.nix
+++ home/tui/neovim/presence.nix
@@ -3,27 +3,31 @@
   programs.nvf.settings.vim.extraPlugins.cord = {
     package = pkgs.vimPlugins.cord-nvim;
     setup = ''
+      local diagnostics = require("cord.plugins.diagnostics")
+      diagnostics.config.scope = 0
+      diagnostics.config.severity = { min = vim.diagnostic.severity.HINT }
+      diagnostics.config.override = true
+
+      local scoped_timestamps = require("cord.plugins.scoped_timestamps")
+      scoped_timestamps.config.scope = "workspace"
+      scoped_timestamps.config.pause = true
+
       require("cord").setup({
         editor = {
           tooltip = "Editing with Neovim",
         },
         display = {
           flavor = "accent",
-          view = "auto",
         },
-        timestamp = {
-          shared = true,
+        plugins = {
+          "cord.plugins.diagnostics",
+          "cord.plugins.scoped_timestamps",
         },
         advanced = {
           discord = {
             reconnect = {
               enabled = true,
             },
-          },
-        },
-        plugins = {
-          ["cord.plugins.diagnostics"] = {
-            severity = vim.diagnostic.severity.HINT,
           },
         },
       })
